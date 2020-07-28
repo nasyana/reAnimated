34c8177007b147ec354d3a554cc9ef9e4d5bd8a2
Usama <iamusama545@gmail.com>
2020-07-28 17:45:35 +0500

    Third

--- App.js
+++ App.js
@@ -1,50 +1,54 @@
-import React, { Component } from 'react';
-import { View ,StyleSheet, ScrollView} from 'react-native';
 
-export default class App extends Component {
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Animated from "react-native-reanimated";
+import { TapGestureHandler, State, ScrollView } from "react-native-gesture-handler";
+
+const { event, cond, eq, Value } = Animated;
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const state = new Value(-1);
+
+    this.onStateChange = event([{
+      nativeEvent: {
+        state: state,
+      },
+    }]);
+
+  this._width = cond(eq(state, State.BEGAN), 300, 200);
+  }
   render() {
     return (
-
+      <View style={styles.container}>
+        <ScrollView horizontal={true}>      
+            <TapGestureHandler onHandlerStateChange={this.onStateChange}>
+          <Animated.View style={[styles.box, { width: this._width }]} />
+          
+        </TapGestureHandler>
       
+        <View style={{width:100,height:100,backgroundColor:'blue'}}/>
+          <View style={{width:100,height:100,backgroundColor:'green'}}/>
+          </ScrollView>
 
-    
-      <View
-        style={styles.container}>
-        <ScrollView horizontal={true}>
-        <View style={styles.box1 }/>
-        <View style={styles.box2} />
-        <View style={styles.box3} />
-        
-        </ScrollView>
       </View>
       
-
     );
   }
 }
 
-
 const styles = StyleSheet.create({
-
-  container:{
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
+  container: {
+    flex: 1,
+    flexDirection:"row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
-  box1:{
-    width: 100, height: 100, backgroundColor: 'powderblue' 
-
+  box: {
+    backgroundColor: "tomato",
+    width: 200,
+    height: 200,
   },
-  box2:
-  {
-    height: 100,width:100, backgroundColor: 'red'
-  },
-
-  box3:
-  {
-    height: 100,width:100, backgroundColor: 'steelblue'
-
-  }
-  
-})
+});
